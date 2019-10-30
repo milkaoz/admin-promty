113f228fd65c65f1c70d93dae82a59ad94807039
milkaoz <milkaoz@gmail.com>
2019-10-30 01:30:18 -0300

    Componentes grafico Donas y incrementador con @Input, @OutPut y @ViewChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 // Rutas
 import { APP_ROUTES } from './app.routes';
+
+// Modulos
 import { PagesModule } from './pages/pages.module';
-
-
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
 
 @NgModule({
   declarations: [
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PAGES_ROUTES } from './pages.routes';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
+
+// ng2- charts
+import { ChartsModule } from 'ng2-charts';
 
 import { PagesComponent } from './pages.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
@@ -17,11 +24,15 @@ import { PAGES_ROUTES } from './pages.routes';
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
     PAGES_ROUTES,
+    FormsModule,
+    ChartsModule,
   ],
   exports: [
     DashboardComponent,
