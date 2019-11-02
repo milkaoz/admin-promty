2e45b851a837cd1e978213df93439349fdff0724
milkaoz <milkaoz@gmail.com>
2019-11-01 22:43:51 -0300

    Se agregan promesas, metatag

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: []
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() {
+
+    this.contarTres().then(
+      mensaje  => console.log('Termino', mensaje)
+      // () => console.log('Termino!')
+      // () => console.log('Error!')
+    )
+    .catch( error => console.error('Error en la promesa', error ));
+
+  }
+
+  ngOnInit() {
+  }
+
+  contarTres(): Promise<boolean> {
+    return new Promise ( ( resolve, reject ) => {
+
+      let contador = 0;
+
+      let intervalo = setInterval( () => {
+
+        contador += 1;
+        console.log( contador );
+
+        if ( contador === 3 ) {
+          resolve(true);
+          // reject('Simplemente un error');
+          clearInterval(intervalo);
+        }
+      }, 100 );
+
+    });
+  }
+}
